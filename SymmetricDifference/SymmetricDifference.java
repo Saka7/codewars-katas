ed04a3966101e6a888fc742302785f7dfee780e4
Saka7 <saka71996@gmail.com>
2016-06-11 21:52:40 +0300

    Removing solution

--- SymmetricDifference/SymmetricDifference.java
+++ SymmetricDifference/SymmetricDifference.java
@@ -6,23 +6,8 @@ import java.util.Set;
 public class SymmetricDifference {
 
 	public static Set<Integer> getSymmetricDifference(final Set<Integer> a, final Set<Integer> b) {
-		if(a == null || b == null) return null;
-		
-		if(a.isEmpty())
-			if(b.isEmpty())
-				return new HashSet<>();
-			else 
-				return b;
-		
-		Set<Integer> symmetricDifference = new HashSet<>(a);
-		symmetricDifference.addAll(b);
-		
-		Set<Integer> intersection = new HashSet<>(a);
-		intersection.retainAll(b);
-		
-		symmetricDifference.removeAll(intersection);
-		
-		return symmetricDifference;
+		// Do your magic
+		return null;
 	}
 	
 }
